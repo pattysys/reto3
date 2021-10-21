df954fcae624b509a95d46cbe4f54fedfd5f302e
pattysys <sandra.munevar.mt@correo.usa.edu.co>
2021-10-21 08:14:55 -0500

    se actualiza reto 4

--- src/main/java/com/demo/rentortopedic/model/Ortopedic.java
+++ src/main/java/com/demo/rentortopedic/model/Ortopedic.java
@@ -1,39 +1,94 @@
 package com.demo.rentortopedic.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import javax.persistence.*;
+import javax.persistence.*;//<rule ref="category/java/bestpractices.xml/UnusedImports" />
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ *
+ *
+ * @author Patricia Munevar
+ */
 @Entity
 @Table(name = "ortopedic")
+/**
+ *
+ *
+ * Clase publica
+ */
 public class Ortopedic implements Serializable {
 
 
-
+    /**
+     *
+     *
+     * Metodo para generar valores de la tabla
+     */
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    /**
+     *
+     *
+     * anotacion para ignorar
+     */
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// anotacion llave primaria autoincrementable
     private Integer id;
     private String name;
     private String brand;
     private Integer year;
     private String description;
 
+    /**
+     *
+     *
+     * Relacion muchos a unos category
+     */
     @ManyToOne
+    /**
+     *
+     *
+     * anotacion para unir las columnas
+     */
     @JoinColumn(name = "idCategory")
+    /**
+     *
+     *
+     * anotacion para ignorar
+     */
     @JsonIgnoreProperties("ortopedics")
     private Category category;
 
-
+    /**
+     *
+     *
+     * Relacion muchos a unos
+     */
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "ortopedic")
-    @JsonIgnoreProperties({"ortopedic","client"})
+    /**
+     *
+     *
+     * anotacion para ignorar
+     */
+    @JsonIgnoreProperties({"ortopedic","client"})//ignorar ortopetic, client
     private List<Message> messages;
-
+    /**
+     *
+     *
+     * Relacion muchos a unos
+     */
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "ortopedic")
-    @JsonIgnoreProperties({"ortopedic","messages"})
+    /**
+     *
+     *
+     * anotacion para ignorar
+     */
+    @JsonIgnoreProperties({"ortopedic","messages"})//ignorar ortopedic, messages
     public List<Reservation> reservations;
-
+    /**
+     *
+     *
+     * Getters and setters
+     */
     public Integer getId() {
         return id;
     }
@@ -41,9 +96,11 @@ public class Ortopedic implements Serializable {
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
